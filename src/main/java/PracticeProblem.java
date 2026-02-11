65c2bbdff637d7a9defe1ccc077b323304ffe729
ericgranitewang <166552552+ericgranitewang@users.noreply.github.com>
2026-02-11 14:47:12 +0000

    commit real

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,61 @@ public class PracticeProblem {
 
 	}
 
-	//Write your functions here
+	public static String evenOrOdd(int a) {
+		if (a%2==0) {
+			return "Even";
+		} else {
+			return "Odd";
+		}
+	}
 
+	public static String teacherOrStudent(String a) {
+		if (a.equals("Kalisz")) {
+			return "Teacher";
+		} else {
+			return "Student";
+		}
+	}
+
+	public static int fartherFromZero(int a) {
+		if (a > 0) {
+			return a+5;
+		} else if (a < 0) {
+			return a-5;
+		} else {
+			return 0;
+		}
+	}
+
+	public static String isFive(int a) {
+		if (a == 5) {
+			return "The number is Five";
+		} else {
+			return "The number is not Five";
+		}
+	}
+
+	public static String positiveOrNegative(double a) {
+		if (a > 0) {
+			return "Positive";
+		} else {
+			return "Negative";
+		}
+	}
+
+	public static String highOrLow(int a) {
+		if (a > 100) {
+			return "High";
+		} else {
+			return "Low";
+		}
+	}
+
+	public static String isHello(String a) {
+		if (a.equals("Hello")) {
+			return "The word is Hello";
+		} else {
+			return "The word is not Hello";
+		}
+	}
 }
